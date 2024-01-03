4b28e5d6499f72907cedf0de5a576f3ff8e08e6e
TitiaC62 <laetitia.crombet@gmail.com>
2024-01-03 01:17:55 +0100

    modif accueil

--- script/accueil.js
+++ script/accueil.js
@@ -15,7 +15,7 @@ const idInterval = setInterval(function(){
     }
 }, interval)
 
-//Book : renvoi vers la video de l'histoire de Noël
+//Renne : renvoi vers la video de l'histoire de Noël
  const reindeer = document.querySelector(".reindeer")
 
  document.querySelector('.video_click').addEventListener('click', function() {
@@ -59,11 +59,18 @@ function updateCountdown() {
       
     const countdownElement = document.getElementById('countdown');
     countdownElement.innerHTML = `Plus que ${days}j ${hours}h ${minutes}Min  avant Noël !`;
+
+    const isMobile = window.innerWidth <= 450; 
+
+    if (isMobile){
+        countdownElement.innerHTML = `J-${days} ${hours}h ${minutes}Min  avant Noël !`;
+    }
 }
 
 
 updateCountdown();
 setInterval(updateCountdown, 1000);
+
 
 
 //Son Pere Noel
@@ -84,67 +91,68 @@ const santa = document.querySelector('.santa')
         })
 
 
+// neige qui tombe
 
-        const createSnowflake = () => {
-            const snowflake = document.createElement('div');
-            snowflake.classList.add('snowflake');
-            snowflake.style.width = '10px';
-            snowflake.style.height = '10px';
-            snowflake.style.top = '-10px';
-            snowflake.style.left = Math.random() * window.innerWidth + 'px';
-            document.body.appendChild(snowflake);
-            return snowflake;
-        };
-        
-        const moveSnowflake = (snowflake) => {
-            const fallSpeed = 3 + Math.random() * 10;
-            const initialPosition = -10;
-            let position = initialPosition;
-        
-            const updatePosition = () => {
-                position += fallSpeed;
-                snowflake.style.top = position + 'px';
-                if (position > window.innerHeight) {
-                   
-                    snowflake.style.left = Math.random() * window.innerWidth + 'px';
-                }
-            };
-        
-            const animate = () => {
-                updatePosition();
-                requestAnimationFrame(animate);
-            };
-        
-            animate();
-        };
-        
-        const createSnowfall = () => {
-            const isMobile = window.innerWidth <= 768; 
-        
-            if (!isMobile) {
-                const NUM_SNOWFLAKES = 100;
-                const snowflakes = [];
-        
-                for (let i = 0; i < NUM_SNOWFLAKES; i++) {
-                    const snowflake = createSnowflake();
-                    snowflakes.push(snowflake);
-                    moveSnowflake(snowflake);
-                }
-        
-                const checkSnowflakes = () => {
-                    snowflakes.forEach((snowflake, index) => {
-                        const position = parseFloat(snowflake.style.top);
-                        if (position > window.innerHeight) {
-                            snowflake.remove();
-                            snowflakes[index] = createSnowflake();
-                            moveSnowflake(snowflakes[index]);
-                        }
-                    });
-                    requestAnimationFrame(checkSnowflakes);
-                };
-        
-                checkSnowflakes();
-            }
-        };
-        
-        createSnowfall();
+const createSnowflake = () => {
+    const snowflake = document.createElement('div');
+    snowflake.classList.add('snowflake');
+    snowflake.style.width = '10px';
+    snowflake.style.height = '10px';
+    snowflake.style.top = '-10px';
+    snowflake.style.left = Math.random() * window.innerWidth + 'px';
+    document.body.appendChild(snowflake);
+    return snowflake;
+ };
+
+const moveSnowflake = (snowflake) => {
+    const fallSpeed = 3 + Math.random() * 10;
+    const initialPosition = -10;
+    let position = initialPosition;
+
+    const updatePosition = () => {
+    position += fallSpeed;
+    snowflake.style.top = position + 'px';
+    if (position > window.innerHeight) {
+
+    snowflake.style.left = Math.random() * window.innerWidth + 'px';
+    }
+    };
+
+    const animate = () => {
+    updatePosition();
+    requestAnimationFrame(animate);
+    };
+
+    animate();
+};
+
+const createSnowfall = () => {
+    const isMobile = window.innerWidth <= 450; 
+
+    if (!isMobile) {
+    const NUM_SNOWFLAKES = 100;
+    const snowflakes = [];
+
+    for (let i = 0; i < NUM_SNOWFLAKES; i++) {
+    const snowflake = createSnowflake();
+    snowflakes.push(snowflake);
+    moveSnowflake(snowflake);
+    }
+
+    const checkSnowflakes = () => {
+    snowflakes.forEach((snowflake, index) => {
+    const position = parseFloat(snowflake.style.top);
+    if (position > window.innerHeight) {
+    snowflake.remove();
+    snowflakes[index] = createSnowflake();
+    moveSnowflake(snowflakes[index]);
+    }
+    });
+    requestAnimationFrame(checkSnowflakes);
+    };
+
+    checkSnowflakes();
+    }
+ };
+
+ createSnowfall();
