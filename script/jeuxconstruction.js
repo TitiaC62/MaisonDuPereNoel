68a428949b9c36fce70d9f5994b8e14a734ba650
TitiaC62 <laetitia.crombet@gmail.com>
2024-11-20 13:52:40 +0100

    add page js jeuxconstruction

---
+++ script/jeuxconstruction.js
@@ -0,0 +1,11 @@
+// Mettre les class des jeux de constrction en HTML
+
+class Construction {
+    constructor(nom, type, age){
+       this.nom = nom; 
+       this.type = type;
+       this.age = age;
+    }
+
+}
+
