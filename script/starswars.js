bbd926f7c3bbbc2b58e0352dfafe56aa642210a1
TitiaC62 <laetitia.crombet@gmail.com>
2024-11-25 23:22:16 +0100

    add starwars api

--- script/starswars.js
+++ script/starswars.js
@@ -1,14 +1,24 @@
 // API Star wars
-
+let button = document.getElementById("button");
+let image = document.getElementById("image");
+let PeopleGender = document.getElementById("gender");
+let PeopleName = document.getElementById("name");
 
 async function PersoStarWars() {
-    const results = await fetch ("https://swapi.dev/api/films/1/");
-    const data =  results.json();
+    let randomNumber = Math.ceil(Math.random()*82) + 1;
+
+    let results = await fetch (`https://swapi.dev/api/people/${randomNumber}`);
+    let data = await results.json();
 
     console.log(data)
+
+    image.textContent = data.films;
+    PeopleGender.textContent = data.gender;
+    PeopleName.textContent =data.name;
 }
 
 
 
+PersoStarWars();
+button.addEventListener("click", PersoStarWars);
 
-   
