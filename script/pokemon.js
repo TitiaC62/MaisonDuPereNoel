4ae78f17354b13e4763bc57e33d36c067f6bb2d2
TitiaC62 <laetitia.crombet@gmail.com>
2024-12-01 23:40:28 +0100

    add functuin pokemon

--- script/pokemon.js
+++ script/pokemon.js
@@ -1,27 +1,38 @@
-let button = document.getElementById("button");
-let image = document.getElementById("image");
-let pokeNumber = document.getElementById("number");
-let pokeName = document.getElementById("name");
+let button = document.querySelector(".button");
 
-const changePokemon = async () =>{
+
+
     // Math.random => permet de prendre un chiffre en [0,1[ 
     // On multiplie par 151 pour avoir les 151 pokemon
     // Math.ceil => arrondi au dessus donc 149.99 = 150
     // On met +1 pour être sûr d'avoir un chiffre entre 1 et 151
 
-    let randomNumber = Math.ceil(Math.random() * 150) +1;
+async function changePokemon  (element){
+    const randomNumber = Math.ceil(Math.random() * 150) +1;
 
     let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`
 
     let response = await fetch(requestString);
     let data = await response.json()
 
+    let image = document.querySelector(".image");
+    let pokeNumber = document.querySelector(".number");
+    let pokeName = document.querySelector(".name");
+
     image.src = data.sprites.front_default ;
     pokeNumber.textContent = `#${data.id}`;
-    pokeName.textContent = data.name;
-
-        
+    pokeName.textContent = data.name;    
 }
 
-changePokemon();
-button.addEventListener("click", changePokemon);
+const pokemonsEl = document.querySelectorAll("section .button");
+  for (const pokemonEl of pokemonsEl) {
+    changePokemon(pokemonEl.parentElement);
+    pokemonEl.addEventListener("click", () => changePokemon(pokemonEl.parentElement));
+  
+
+}
+
+
+  changePokemon()
+
+
